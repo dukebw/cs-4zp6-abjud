db6e400ba6c30d3fcda28f20b9bd68bb327d140f
davidpitkanen <d.t.pitkanen@gmail.com>
2016-11-23 02:23:52 -0500

    the readme contains most of the info about the files

--- tensorFlowModels/spatialNet.py
+++ tensorFlowModels/spatialNet.py
@@ -9,6 +9,13 @@ Project: https://github.com/aymericdamien/TensorFlow-Examples/
 from __future__ import print_function
 
 import tensorflow as tf
+import random
+from tensorflow.python.framework import ops
+from tensorflow.python.framework import dtypes
+
+
+# you will get as input a 28x28 sized image.
+# a list of 7 key joint positions
 
 # Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
@@ -21,10 +28,120 @@ mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 # the specific numbers they use
 
 
+# to get the data step 1 is to put the data into
+
+# put the data into a tensor of size (num_images, IMAGE_SIZE, IMAGE_SIZE, 1)
+# then you have a list of labels -- something like ideal output
+
+# steps seem to be: 
+
+
+# set the names of the datapaths
+
+dataset_path = ""
+test_labels_file = ""
+train_labels_file = ""
+
+
+# probably want to change most of this
+test_set_size = 5
+IMAGE_HEIGHT = 28
+IMAGE_WIDTH = 28
+NUM_CHANNELS = 3
+
+
+# Creating a pipeline step 1
+# Load the label data which contains lists
+# of input files
+
+def read_label_file(file):
+    f = open(file, 'r')
+    filepaths = []
+    labels = []
+    for line in f:
+        filepath, label = line.split(",") # this is for csv files
+        filepaths.append(filepath)
+        labels.append(encode_label(label))
+    return filepaths, labels
+
+def encode_label(label):
+    return int(label)
+
+# here call both of the above methods
+train_filepaths, train_labels = read_label_file(dataset_path + train_labels_file)
+test_filepaths, test_labels = read_label_file(dataset_path + test_labels_file)
+
+# those are just filenames so next step is to turn them into filepaths
+# combine test and train into a list of all
+
+
+# Step 2 is to do some optional processing on our string lists
+# currently choosing not to bother with this option
+
+
+# Step 3 start building the pipeline
+# they say the data type of the tensor must match that of the lists
+# not sure what this means at the moment
+
+all_images = ops.convert_to_tensor(all_filepaths, dtype=dtypes.string)
+all_labels = ops.convert_to_tensor(all_labels, dtype=dtypes.int32)
+
+
+#make partitions
+
+partitions = [0]*len(all_filepaths)
+partitions[:test_set_size]=[1]*test_set_size
+random.shuffle(partitions)
+
+# partition the data into a test and train set according to our
+# partition vector
+train_images, test_images = tf.dynamic_partition(all_images, partitions,2)
+train_labels, test_labels = tf.dynamic_partition(all_labels, partitions,2)
+
+
+# Create input Queues and defint how to load the images
+
+train_input_queue = tf.train.slice_input_producer(
+    [train_images, train_labels],
+    shuffle=False)
+test_input_queue = tf.train.slice_input_producer(
+    [test_images,test_labels],
+    shuffle=False)
+
+# process and string tensor into an image and a label
+
+file_content = tf.read_file(train_input_queue[0])
+train_image = tf.image.decode_jpeg(file_content, channels=NUM_CHANNELS)
+test_label = test_input_queue[1]
+
+# Group Samples into Batches
+# if run train_image in a session you would get a single image (28,28,1)
+# training 1 at a time is inefficient so want to work in batches
+
+# define tensor shape
+
+train_image.set_shape([IMAGE_HEIGHT, IMAGE_WIDTH, NUM_CHANNELS])
+test_image.set_shape([IMAGE_HEIGHT, IMAGE_WIDTH, NUM_CHANNELS])
+
+#collect batches of images before processing
+train_image_batch, train_label_batch = tf.train.batch(
+    [train_image,train_label],
+    batch_size=BATCH_SIZE
+    #,num_threads =1
+    )
+
+test_image_batch, test_label_batch = tf.train.batch(
+    [test_image,test_label],
+    batch_size=BATCH_SIZE
+    #,num_threads =1
+    )
+
+# Run the queue runners and start a session
+# finished building the input pipeline however if try to acccess
+# 
 
 # for each picture used in training I need 7 coordinates -- one for each joint.
 # create a family of 7 Gaussians using these 7 points
-
 
 learning_rate = 0.001
 training_iters = 200000
@@ -54,6 +171,32 @@ def maxpool2d(x, k=2):
     # MaxPool2D wrapper
     return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
                           padding='SAME')
+
+# all the specifics of the model from caffe
+
+# blobs_lr: 1; blobs_lr: 2; -- individualize the learning rates weights
+# vs biases
+# weight_decay: 1; weight_decay: 0; these are the regularization terms
+# not sure how to specify this in tensorFlow?
+# num_output = 128; kernel_size = 5; stride : 1; pad: 2; basic parameters
+# weight_filler {type: gaussian std: 0.01 -- setting the initial weights
+# bias filler: type constant; value 0
+# then there's a rectilinear unit and max_pooling of size 2
+
+# implement this
+
+
+
+# layer2
+# seems like the same as layer 1 except the number of inputs is different
+
+# layer3
+# details
+# seems the same as above again but there's no pooling
+
+# layer4
+# 
+#
 
 
 # Create model
@@ -169,6 +312,7 @@ with tf.Session() as sess:
     # Keep training until reach max iterations
     while step * batch_size < training_iters:
         batch_x, batch_y = mnist.train.next_batch(batch_size)
+        feed_dict = fill_feed_dict()
         # Run optimization op (backprop)
         sess.run(optimizer, feed_dict={x: batch_x, y: batch_y,
                                        keep_prob: dropout})
