1f3fdc98b78b7e4ed936be71249966b765fa8e8a
Brendan Duke <dukebw@mcmaster.ca>
2016-12-01 09:03:51 -0500

    Created mpii_read file

---
+++ tensorFlowModels/write_tf_record/mpii_read.py
@@ -0,0 +1,14 @@
+"""
+A module for reading the [MPII Human Pose
+Dataset](http://human-pose.mpi-inf.mpg.de/).
+"""
+
+def mpii_read():
+    """
+    Read .mat file and return MpiiDataSet type containing images and
+    annotations.
+    """
+    pass
+
+if __name__ == "__main__":
+    mpii_read()
