b6d668eb5cb5d5c5b0252af5b2729efd432783d5
Brendan Duke <dukebw@mcmaster.ca>
2016-12-01 21:41:07 -0500

    Added MpiiDataSet class

--- tensorFlowModels/write_tf_record/mpii_read.py
+++ tensorFlowModels/write_tf_record/mpii_read.py
@@ -2,13 +2,36 @@
 A module for reading the [MPII Human Pose
 Dataset](http://human-pose.mpi-inf.mpg.de/).
 """
+import sys
+import scipy.io
 
-def mpii_read():
+class MpiiDataset:
     """
-    Read .mat file and return MpiiDataSet type containing images and
-    annotations.
+    Representation of the entire MPII Dataset.
+
+    The annotation description can be found
+    [here](http://human-pose.mpi-inf.mpg.de/#download).
+
+    Currently only the images and person-centric body joint annotations are
+    taken from the dataset.
     """
-    pass
+    def __init__(self, images, joints):
+        self._images = images
+        self._joints = joints
+
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def joints(self):
+        return self._joints
+
+def mpii_read(mpii_dataset_filepath):
+    mpii_dataset_mat = scipy.io.loadmat(mpii_dataset_filepath)
+    mpii_annotations = mpii_dataset_mat['RELEASE']['annolist'][0, 0]
 
 if __name__ == "__main__":
-    mpii_read()
+    assert len(sys.argv) == 1
+
+    mpii_read(sys.argv[0])
