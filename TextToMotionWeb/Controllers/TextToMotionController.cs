c6d3f4e81ecd0b9baec5fb178f19179cc5cca7ac
Udip Patel <patel.udip@gmail.com>
2017-01-08 18:15:17 -0500

    updated frontend, added text-to-motion search page

---
+++ TextToMotionWeb/Controllers/TextToMotionController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TextToMotionWeb.Controllers
+{
+    public class TextToMotionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Search(string Query){
+            ViewData["Title"] = "Search Results";
+            ViewData["Query"] = Query;   
+            return View();
+        }
+    }
+}
