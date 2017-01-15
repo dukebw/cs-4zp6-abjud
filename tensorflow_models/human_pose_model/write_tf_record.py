1ee410c879dbab380f7b1f4899bc148b7550ad0e
Brendan Duke <dukebw@mcmaster.ca>
2017-01-15 12:28:15 -0500

    Changed write_tf_record to only write examples in single_person list. Also added joint is_visible attribute to Examples.

--- tensorflow_models/human_pose_model/write_tf_record.py
+++ tensorflow_models/human_pose_model/write_tf_record.py
@@ -256,6 +256,7 @@ def _extract_labeled_joints(person_joints,
     x_sparse_joints = []
     y_sparse_joints = []
     sparse_joint_indices = []
+    is_visible_list = []
     max_cropped_img_dim = max(cropped_img_shape.x, cropped_img_shape.y)
     abs_image_center = Point(offsets.x + cropped_img_shape.x/2,
                              offsets.y + cropped_img_shape.y/2)
@@ -263,7 +264,6 @@ def _extract_labeled_joints(person_joints,
     for joint_index in range(len(person_joints)):
         joint = person_joints[joint_index]
         if joint is not None:
-            joint = Point(joint[0], joint[1])
             if ((offsets.x <= joint.x <= (offsets.x + cropped_img_shape.x)) and
                 (offsets.y <= joint.y <= (offsets.y + cropped_img_shape.y))):
                 _append_scaled_joint(x_sparse_joints,
@@ -277,23 +277,21 @@ def _extract_labeled_joints(person_joints,
 
                 sparse_joint_indices.append(joint_index)
 
-    return x_sparse_joints, y_sparse_joints, sparse_joint_indices
+                is_visible_list.append(joint.is_visible)
+
+    return x_sparse_joints, y_sparse_joints, sparse_joint_indices, is_visible_list
 
 
 def _find_person_bounding_box(person, img_shape):
     """Finds an enclosing bounding box for `person` in the image with
     `img_shape` dimensions.
 
-    Currently the bounding box is found by taking a generous multiple of the
-    size of the person's head bounding box, which is encoded in the `Person`
-    object.
+    Currently the bounding box is found by taking the
+    (scale*200px) by (scale*200px) rectangle centered around `objpos`.
 
-    This has the issue of potentially cropping people in weird positions, for
-    example upside-down people.
-
-    One improvement would be to take the bounding box found with the current
-    method, and expand each dimension so that all the labelled joints are
-    contained.
+    One experiment would be to take the bounding box found with the current
+    method, and expand or shrink each dimension to the minimum spanning
+    rectangle such that all the labelled joints are contained.
 
     Args:
         person: Person to find bounding box for.
@@ -373,7 +371,7 @@ def _write_example(coder, image_jpeg, people_in_img, writer):
             person_shape_xy,
             padding_xy,
             person_rect.top_left)
-        x_sparse_joints, y_sparse_joints, sparse_joint_indices = labels
+        x_sparse_joints, y_sparse_joints, sparse_joint_indices, is_visible_list = labels
 
         example = tf.train.Example(
             features=tf.train.Features(
@@ -381,7 +379,8 @@ def _write_example(coder, image_jpeg, people_in_img, writer):
                     'image_jpeg': _bytes_feature(scaled_img_jpeg),
                     'joint_indices': _int64_feature(sparse_joint_indices),
                     'x_joints': _float_feature(x_sparse_joints),
-                    'y_joints': _float_feature(y_sparse_joints)
+                    'y_joints': _float_feature(y_sparse_joints),
+                    'is_visible_list': _int64_feature(is_visible_list)
                 }))
         writer.write(example.SerializeToString())
 
