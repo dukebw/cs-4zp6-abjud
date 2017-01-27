da7b4bf8cf626f9479cf62f85d3f27ba20e0f6ac
Brendan Duke <dukebw@mcmaster.ca>
2017-01-27 09:34:04 -0500

    Fix bug in resize_images call

--- tensorflow_models/human_pose_model/vgg_bulat.py
+++ tensorflow_models/human_pose_model/vgg_bulat.py
@@ -63,54 +63,55 @@ def vgg_16(inputs,
            is_training=True,
            dropout_keep_prob=0.5,
            scope='vgg_16'):
-  """Oxford Net VGG 16-Layers version D Example.
+    """Oxford Net VGG 16-Layers version D Example.
 
-  Note: All the fully_connected layers have been transformed to conv2d layers.
-        To use in classification mode, resize input to 224x224.
+    Note: All the fully_connected layers have been transformed to conv2d layers.
+          To use in classification mode, resize input to 224x224.
 
-  Args:
-    inputs: a tensor of size [batch_size, height, width, channels].
-    num_classes: number of predicted classes (joints).
-    is_training: whether or not the model is being trained.
-    dropout_keep_prob: the probability that activations are kept in the dropout
-      layers during training.
-    scope: Optional scope for the variables.
+    Args:
+      inputs: a tensor of size [batch_size, height, width, channels].
+      num_classes: number of predicted classes (joints).
+      is_training: whether or not the model is being trained.
+      dropout_keep_prob: the probability that activations are kept in the dropout
+        layers during training.
+      scope: Optional scope for the variables.
 
-  Returns:
-    the last op containing the log predictions and end_points dict.
-  """
-  with tf.variable_scope(scope, 'vgg_16', [inputs]) as sc:
-    end_points_collection = sc.original_name_scope + '_end_points'
-    # Collect outputs for conv2d, fully_connected and max_pool2d.
-    with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
-                        outputs_collections=end_points_collection):
-      net = slim.repeat(inputs, 2, slim.conv2d, 64, [3, 3], scope='conv1')
-      net = slim.max_pool2d(net, [2, 2], scope='pool1')
-      net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
-      net = slim.max_pool2d(net, [2, 2], scope='pool2')
-      net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
-      net = slim.max_pool2d(net, [2, 2], scope='pool3')
-      net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv4')
-      net = slim.max_pool2d(net, [2, 2], scope='pool4')
-      net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv5')
-      net = slim.max_pool2d(net, [2, 2], scope='pool5')
-      # Use conv2d instead of fully_connected layers.
-      net = slim.conv2d(net, 4096, [7, 7], scope='fc6')
-      net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
-                         scope='dropout6')
-      net = slim.conv2d(net, 4096, [1, 1], scope='fc7')
-      net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
-                         scope='dropout7')
-      net = slim.conv2d(net, num_classes, [1, 1],
-                        activation_fn=None,
-                        normalizer_fn=None,
-                        scope='fc8')
-      # TODO(brendan): don't hard-code resized size.
-      net = tf.image.resize_images(images=net,
-                                   size=[380, 380],
-                                   method=ResizeMethod.BILINEAR,
-                                   align_corners=False)
-      # Convert end_points_collection into a end_point dict.
-      end_points = slim.utils.convert_collection_to_dict(end_points_collection)
-      return net, end_points
+    Returns:
+      the last op containing the log predictions and end_points dict.
+    """
+    with tf.variable_scope(scope, 'vgg_16', [inputs]) as sc:
+        end_points_collection = sc.original_name_scope + '_end_points'
+      # Collect outputs for conv2d, fully_connected and max_pool2d.
+        with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
+                            outputs_collections=end_points_collection):
+            input_img_shape = inputs.get_shape()[1:3]
+
+            net = slim.repeat(inputs, 2, slim.conv2d, 64, [3, 3], scope='conv1')
+            net = slim.max_pool2d(net, [2, 2], scope='pool1')
+            net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
+            net = slim.max_pool2d(net, [2, 2], scope='pool2')
+            net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
+            net = slim.max_pool2d(net, [2, 2], scope='pool3')
+            net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv4')
+            net = slim.max_pool2d(net, [2, 2], scope='pool4')
+            net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv5')
+            net = slim.max_pool2d(net, [2, 2], scope='pool5')
+
+            # Use conv2d instead of fully_connected layers.
+            net = slim.conv2d(net, 4096, [7, 7], scope='fc6')
+            net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+                             scope='dropout6')
+            net = slim.conv2d(net, 4096, [1, 1], scope='fc7')
+            net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+                             scope='dropout7')
+            net = slim.conv2d(net, num_classes, [1, 1],
+                            activation_fn=None,
+                            normalizer_fn=None,
+                            scope='fc8')
+            net = tf.image.resize_images(images=net, size=input_img_shape)
+
+            # Convert end_points_collection into a end_point dict.
+            end_points = slim.utils.convert_collection_to_dict(end_points_collection)
+
+            return net, end_points
 vgg_16.default_image_size = 380
