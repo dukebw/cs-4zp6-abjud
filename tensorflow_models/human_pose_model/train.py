b00550f23f8106434c2775a557fdfc9ba0c71205
Thor Jonsson <thor.h.jonsson@gmail.com>
2017-02-01 23:30:19 -0500

    fixed bug in vgg_bulat, added logit summaries to tensorboard

--- tensorflow_models/human_pose_model/train.py
+++ tensorflow_models/human_pose_model/train.py
@@ -22,23 +22,29 @@ RMSPROP_EPSILON = 1.0
 
 NUM_JOINTS = Person.NUM_JOINTS
 
-tf.app.flags.DEFINE_string('network_name', None,
+tf.app.flags.DEFINE_string('network_name', 'vgg_bulat',
                            """Name of desired network to use for part
                            detection. Valid options: vgg, inception_v3.""")
+
 tf.app.flags.DEFINE_string('data_dir', './train_vgg_fcn',
                            """Path to take input TFRecord files from.""")
-tf.app.flags.DEFINE_string('log_dir', './log',
+
+tf.app.flags.DEFINE_string('log_dir', '/mnt/data/datasets/MPII_HumanPose/logs/vgg_bulat/lr1e-4_bs16',
                            """Path to take summaries and checkpoints from, and
                            write them to.""")
+
 tf.app.flags.DEFINE_string('log_filename', 'train_log',
                            """Name of file to log training steps and loss
                            to.""")
-tf.app.flags.DEFINE_string('checkpoint_path', None,
+
+tf.app.flags.DEFINE_string('checkpoint_path', 'checkpoints/vgg_16.ckpt',
                            """Path to take checkpoint file (e.g.
                            inception_v3.ckpt) from.""")
-tf.app.flags.DEFINE_string('checkpoint_exclude_scopes', None,
+
+tf.app.flags.DEFINE_string('checkpoint_exclude_scopes', 'vgg_16/skip',
                            """Comma-separated list of scopes to exclude when
                            restoring from a checkpoint.""")
+
 tf.app.flags.DEFINE_string('trainable_scopes', 'vgg_16/fc6, vgg_16/fc7, vgg_16/fc8, vgg_16/skip',
                            """Comma-separated list of scopes to train.""")
 
@@ -48,34 +54,43 @@ tf.app.flags.DEFINE_integer('image_dim', 380,
 tf.app.flags.DEFINE_integer('num_preprocess_threads', 4,
                             """Number of threads to use to preprocess
                             images.""")
+
 tf.app.flags.DEFINE_integer('num_readers', 4,
                             """Number of threads to use to read example
                             protobufs from TFRecords.""")
+
 tf.app.flags.DEFINE_integer('num_gpus', 1,
                             """Number of GPUs in system.""")
 
-tf.app.flags.DEFINE_integer('batch_size', 32,
+tf.app.flags.DEFINE_integer('batch_size', 16,
                             """Size of each mini-batch (number of examples
                             processed at once).""")
+
 tf.app.flags.DEFINE_integer('input_queue_memory_factor', 16,
                             """Factor by which to increase the minimum examples
                             in RandomShuffleQueue.""")
-tf.app.flags.DEFINE_integer('max_epochs', 100,
+
+tf.app.flags.DEFINE_integer('max_epochs', 30,
                             """Maximum number of epochs in training run.""")
 
 tf.app.flags.DEFINE_integer('heatmap_stddev_pixels', 5,
                             """Standard deviation of Gaussian joint heatmap, in
                             pixels.""")
+
 tf.app.flags.DEFINE_float('initial_learning_rate', 0.1,
                           """Initial learning rate.""")
+
 tf.app.flags.DEFINE_float('learning_rate_decay_factor', 0.16,
                           """Rate at which learning rate is decayed.""")
+
 tf.app.flags.DEFINE_float('num_epochs_per_decay', 30.0,
                           """Number of epochs before decay factor is applied
                           once.""")
+
 tf.app.flags.DEFINE_boolean('restore_global_step', False,
                             """Set to True if restoring a training run that is
                             part-way complete.""")
+
 def _summarize_bulat_model(endpoints):
     """Summarizes the activation values that are marked for summaries in the
     Inception v3 network.
--- tensorflow_models/human_pose_model/vgg_bulat.py
+++ tensorflow_models/human_pose_model/vgg_bulat.py
@@ -83,13 +83,9 @@ def vgg_16_base(inputs, num_classes, dropout_keep_prob, is_training):
     a7 = slim.dropout(a7, dropout_keep_prob, is_training=is_training,
                       scope='dropout7')
 
-    return slim.conv2d(a7,
-                       num_classes,
-                       [1, 1],
-                       activation_fn=None,
-                       normalizer_fn=None,
-                       scope='fc8')
+    a8  = slim.conv2d(a7, num_classes, [1, 1], activation_fn=None, normalizer_fn=None, scope='fc8')
 
+    return a3, a4, a8
 
 def vgg_16_fcn32(inputs,
                  num_classes=16,
@@ -104,7 +100,7 @@ def vgg_16_fcn32(inputs,
         # Collect outputs for conv2d, fully_connected and max_pool2d.
         with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
                             outputs_collections=end_points_collection):
-            a8 = vgg_16_base(inputs,
+            _, _, a8 = vgg_16_base(inputs,
                              num_classes,
                              dropout_keep_prob,
                              is_training)
@@ -144,7 +140,7 @@ def vgg_16(inputs,
         # Collect outputs for conv2d, fully_connected and max_pool2d.
         with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
                             outputs_collections=end_points_collection):
-            a8 = vgg_16_base(inputs,
+            a3, a4, a8 = vgg_16_base(inputs,
                              num_classes,
                              dropout_keep_prob,
                              is_training)
