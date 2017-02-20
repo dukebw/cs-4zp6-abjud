a2e09e66b6c8247f4acbc5b44022c597340e4a88
thor <tjonsson@uoguelph.ca>
2017-02-20 12:39:57 -0500

    modified the pose_flags to represent the evaluation config needed for the current running best performer wrt training_loss, I can confirm that code works

--- tensorflow_models/human_pose_model/pose_flags.py
+++ tensorflow_models/human_pose_model/pose_flags.py
@@ -2,22 +2,24 @@ import tensorflow as tf
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('network_name', 'vgg_bulat',
+tf.app.flags.DEFINE_string('network_name', 'resnet_bulat',
                            """Name of desired network to use for part
                            detection. Valid options: vgg, inception_v3.""")
 
 tf.app.flags.DEFINE_string('loss_name', 'mean_squared_error_loss',
                            """Name of desired loss function to use.""")
 
-tf.app.flags.DEFINE_string('train_data_dir', './train_vgg_fcn',
+tf.app.flags.DEFINE_string('train_data_dir',
+                           '/tmp/MPII_Data/train_80_shards_w_binmap',
                            """Path to take input training TFRecord files
                            from.""")
 
-tf.app.flags.DEFINE_string('validation_data_dir', './valid_vgg_fcn',
+tf.app.flags.DEFINE_string('validation_data_dir',
+                           '/tmp/MPII_Data/valid_80_shards_w_binmap',
                            """Path to take input validation TFRecord files
                            from.""")
 
-tf.app.flags.DEFINE_string('log_dir', '/mnt/data/datasets/MPII_HumanPose/logs/vgg_bulat/lr1e-4_bs16',
+tf.app.flags.DEFINE_string('log_dir', 'logs/resnet/exp0',
                            """Path to take summaries and checkpoints from, and
                            write them to.""")
 
@@ -25,18 +27,20 @@ tf.app.flags.DEFINE_string('log_filename', 'train_log',
                            """Name of file to log training steps and loss
                            to.""")
 
-tf.app.flags.DEFINE_string('checkpoint_path', 'checkpoints/vgg_16.ckpt',
+tf.app.flags.DEFINE_string('checkpoint_path',
+                           'logs/resnet/exp0/model.ckpt-22992',
                            """Path to take checkpoint file (e.g.
                            inception_v3.ckpt) from.""")
 
-tf.app.flags.DEFINE_string('checkpoint_exclude_scopes', None,
+tf.app.flags.DEFINE_string('checkpoint_exclude_scopes',
+                           'block3b,logits,b7_deconv,biases',
                            """Comma-separated list of scopes to exclude when
                            restoring from a checkpoint.""")
 
 tf.app.flags.DEFINE_string('trainable_scopes', None,
                            """Comma-separated list of scopes to train.""")
 
-tf.app.flags.DEFINE_integer('image_dim', 380,
+tf.app.flags.DEFINE_integer('image_dim', 256,
                             """Dimension of the square input image.""")
 
 tf.app.flags.DEFINE_integer('num_preprocess_threads', 4,
@@ -47,18 +51,18 @@ tf.app.flags.DEFINE_integer('num_readers', 4,
                             """Number of threads to use to read example
                             protobufs from TFRecords.""")
 
-tf.app.flags.DEFINE_integer('num_gpus', 3,
+tf.app.flags.DEFINE_integer('num_gpus', 4,
                             """Number of GPUs in system.""")
 
-tf.app.flags.DEFINE_integer('batch_size', 16,
+tf.app.flags.DEFINE_integer('batch_size', 32,
                             """Size of each mini-batch (number of examples
                             processed at once).""")
 
-tf.app.flags.DEFINE_integer('input_queue_memory_factor', 16,
+tf.app.flags.DEFINE_integer('input_queue_memory_factor', 4,
                             """Factor by which to increase the minimum examples
                             in RandomShuffleQueue.""")
 
-tf.app.flags.DEFINE_integer('max_epochs', 30,
+tf.app.flags.DEFINE_integer('max_epochs', 90,
                             """Maximum number of epochs in training run.""")
 
 tf.app.flags.DEFINE_integer('heatmap_stddev_pixels', 5,
@@ -69,7 +73,7 @@ tf.app.flags.DEFINE_integer('eval_interval_secs', 360,
                             between checking for new checkpoints and evaluating
                             them.""")
 
-tf.app.flags.DEFINE_float('initial_learning_rate', 0.1,
+tf.app.flags.DEFINE_float('initial_learning_rate', 0.01,
                           """Initial learning rate.""")
 
 tf.app.flags.DEFINE_float('learning_rate_decay_factor', 0.16,
@@ -79,6 +83,6 @@ tf.app.flags.DEFINE_float('num_epochs_per_decay', 30.0,
                           """Number of epochs before decay factor is applied
                           once.""")
 
-tf.app.flags.DEFINE_boolean('restore_global_step', False,
+tf.app.flags.DEFINE_boolean('restore_global_step', True,
                             """Set to True if restoring a training run that is
                             part-way complete.""")
