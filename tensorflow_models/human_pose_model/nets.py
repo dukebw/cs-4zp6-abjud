404671c235b8330ad48622a73b741588bf53de3f
Brendan Duke <dukebw@mcmaster.ca>
2017-02-20 19:13:22 -0500

    Fix for loss calculations (don't divide out image size)

--- tensorflow_models/human_pose_model/nets.py
+++ tensorflow_models/human_pose_model/nets.py
@@ -2,19 +2,6 @@
 Slim where needed.
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from tensorflow.python.framework import ops
-from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import confusion_matrix
-from tensorflow.python.ops import control_flow_ops
-from tensorflow.python.ops import math_ops
-from tensorflow.python.ops import nn
-from tensorflow.python.ops import nn_ops
-from tensorflow.python.ops import weights_broadcast_ops
-from tensorflow.python.ops.losses import util
 from tensorflow.contrib.slim.nets import inception
 from tensorflow.contrib.slim.nets import vgg
 
@@ -36,126 +23,6 @@ def _summarize_loss(total_loss, gpu_index):
         total_loss = tf.identity(total_loss)
 
     return total_loss
-
-def _scale_losses(losses, weights):
-  	"""Computes the scaled loss.
-  	Args:
-  	  losses: `Tensor` of shape `[batch_size, d1, ... dN]`.
-  	  weights: `Tensor` of shape `[]`, `[batch_size]` or
-  	    `[batch_size, d1, ... dN]`. The `losses` are reduced (`tf.reduce_sum`)
-  	    until its dimension matches that of `weights` at which point the reduced
-  	    `losses` are element-wise multiplied by `weights` and a final `reduce_sum`
-  	    is computed on the result. Conceptually, this operation is similar to
-  	    broadcasting (tiling) `weights` to be the same shape as `losses`,
-  	    performing an element-wise multiplication, and summing the result. Note,
-  	    however, that the dimension matching is right-to-left, not left-to-right;
-  	    i.e., the opposite of standard NumPy/Tensorflow broadcasting.
-  	Returns:
-  	  A scalar tf.float32 `Tensor` whose value represents the sum of the scaled
-  	    `losses`.
-  	"""
-  	weighted_losses = math_ops.multiply(losses, weights)
-  	return math_ops.reduce_sum(weighted_losses)
-
-
-def _safe_div(numerator, denominator, name="value"):
-  	"""Computes a safe divide which returns 0 if the denominator is zero.
-  	Note that the function contains an additional conditional check that is
-  	necessary for avoiding situations where the loss is zero causing NaNs to
- 	 creep into the gradient computation.
-
-  	Args:
-  	  numerator: An arbitrary `Tensor`.
-  	  denominator: `Tensor` whose shape matches `numerator` and whose values are
-  	    assumed to be non-negative.
-  	  name: An optional name for the returned op.
-  	Returns:
-  	  The element-wise value of the numerator divided by the denominator.
-  	"""
-  	return array_ops.where(math_ops.greater(denominator, 0),
-  	    				   math_ops.div(numerator,
-						   array_ops.where(math_ops.equal(denominator, 0),
-  	        			   array_ops.ones_like(denominator),
-						   denominator)),
-  	    				   array_ops.zeros_like(numerator),
-  	    				   name=name)
-
-
-def _safe_mean(losses, num_present):
-    """Computes a safe mean of the losses.
-    Args:
-        losses: `Tensor` whose elements contain individual loss measurements.
-        num_present: The number of measurable elements in `losses`.
-    Returns:
-        A scalar representing the mean of `losses`. If `num_present` is zero,
-        then zero is returned.
-    """
-    total_loss = math_ops.reduce_sum(losses)
-    return _safe_div(total_loss, num_present)
-
-
-def _num_present(losses, weights, per_batch=False):
-    """Computes the number of elements in the loss function induced by `weights`.
-    A given weights tensor induces different numbers of usable elements in the
-    `losses` tensor. The `weights` tensor is broadcast across `losses` for all
-    possible dimensions. For example, if `losses` is a tensor of dimension
-    `[4, 5, 6, 3]` and `weights` is a tensor of shape `[4, 5]`, then `weights` is,
-    in effect, tiled to match the shape of `losses`. Following this effective
-    tile, the total number of present elements is the number of non-zero weights.
-    Args:
-      losses: `Tensor` of shape `[batch_size, d1, ... dN]`.
-      weights: `Tensor` of shape `[]`, `[batch_size]` or
-        `[batch_size, d1, ... dK]`, where K < N.
-      per_batch: Whether to return the number of elements per batch or as a sum
-        total.
-    Returns:
-      The number of present (non-zero) elements in the losses tensor. If
-        `per_batch` is `True`, the value is returned as a tensor of size
-        `[batch_size]`. Otherwise, a single scalar tensor is returned.
-    """
-    with ops.name_scope(None, "num_present", (losses, weights)) as scope:
-        weights = math_ops.to_float(weights)
-        present = array_ops.where(math_ops.equal(weights, 0.0), array_ops.zeros_like(weights), array_ops.ones_like(weights))
-		present = weights_broadcast_ops.broadcast_weights(present, losses)
-        if per_batch:
-            return math_ops.reduce_sum(present, axis=math_ops.range(1, array_ops.rank(present)), keep_dims=True, name=scope)
-        return math_ops.reduce_sum(present, name=scope)
-
-
-def compute_weighted_loss(losses, 
-						  weights,
-						  scope=None, 
-						  loss_collection=ops.GraphKeys.LOSSES):
-  	"""Computes the weighted loss.
-  	Args:
-    	losses: `Tensor` of shape `[batch_size, d1, ... dN]`.
-    	weights: Optional `Tensor` whose rank is either 0, or the same rank as
-    	  `losses`, and must be broadcastable to `losses` (i.e., all dimensions must
-    	  be either `1`, or the same as the corresponding `losses` dimension).
-    	scope: the scope for the operations performed in computing the loss.
-    	loss_collection: the loss will be added to these collections.
-  	Returns:
-  	  A scalar `Tensor` that returns the weighted loss.
-  	Raises:
-  	  ValueError: If `weights` is `None` or the shape is not compatible with
-  	    `losses`, or if the number of dimensions (rank) of either `losses` or
-  	    `weights` is missing.
-  	"""
-	# We broadcast down via reduce_sum per image
-	weights = tf.concat(tf.ones(shape = losses.get_shape()[0], weights)
-  	with ops.name_scope(scope, "weighted_loss", (losses, weights)):
-  	  	with ops.control_dependencies((weights_broadcast_ops.assert_broadcastable(weights, losses),)):
-			losses = ops.convert_to_tensor(losses)
-  	  	  	input_dtype = losses.dtype
-  	  	  	losses = math_ops.to_float(losses)
-  	  	  	weights = math_ops.to_float(weights)
-  	  	  	total_loss = _scale_losses(losses, weights)
-  	  	  	num_present = _num_present(losses, weights)
-  	  	  	mean_loss = _safe_mean(total_loss, num_present)
-  	  	  	# Convert the result back to the input type.
-  	  	  	mean_loss = math_ops.cast(mean_loss, input_dtype)
-  	  	  	util.add_loss(mean_loss, loss_collection)
-			return mean_loss
 
 
 def inference(images,
@@ -202,8 +69,12 @@ def inference(images,
                 net_loss(logits, endpoints, heatmaps, binary_maps, weights)
 
             losses = tf.get_collection(key=tf.GraphKeys.LOSSES, scope=scope)
-            regularization_losses = tf.get_collection(key=tf.GraphKeys.REGULARIZATION_LOSSES, scope=scope)
-            total_loss = tf.add_n(inputs=losses + regularization_losses + user_loss, name='total_loss')
+
+            regularization_losses = tf.get_collection(
+                key=tf.GraphKeys.REGULARIZATION_LOSSES, scope=scope)
+
+            total_loss = tf.add_n(inputs=losses + regularization_losses,
+                                  name='total_loss')
             total_loss = _summarize_loss(total_loss, gpu_index)
 
     return total_loss, logits
@@ -228,33 +99,52 @@ def inception_v3_loss(logits, endpoints, dense_joints, weights):
                                  weights=weights)
 
 
+def _add_weighted_loss_to_collection(losses, weights):
+    """Weights `losses` by weights, and adds the weighted losses, normalized by
+    the number of joints present, to `tf.GraphKeys.LOSSES`.
+
+    Specifically, the losses are summed across all dimensions (x, y,
+    num_joints), producing a scalar loss per batch. That scalar loss then needs
+    to be normalized by the number of joints present. This is equivalent to
+    sum(weights[:, 0, 0, :]), since `weights` is a [image_dim, image_dim] map
+    of eithers all 1s or all 0s, depending on whether a joints is present or
+    not, respectively.
+
+    Args:
+        losses: Element-wise losses as calculated by your favourite function.
+        weights: Element-wise weights.
+    """
+    weights = tf.cast(weights, tf.float32)
+    losses = tf.multiply(losses, weights)
+    losses = tf.reduce_sum(input_tensor=losses, axis=[1, 2, 3])
+
+    joints_present_map = weights[:, 0, 0, :]
+    num_joints_present = tf.reduce_sum(input_tensor=joints_present_map, axis=1)
+    tf.assert_greater(num_joints_present, 0.0)
+
+    losses /= num_joints_present
+    total_loss = tf.reduce_sum(input_tensor=losses)
+    tf.add_to_collection(name=tf.GraphKeys.LOSSES, value=total_loss)
+
+
 def sigmoid_cross_entropy_loss(logits, endpoints, heatmaps, binary_maps, weights):
     """Pixelwise cross entropy between binary masks and logits for each channel.
 
     See equation 1 in Bulat paper.
     """
-    """ Does not work because it reduces the mean across all dimensions
-    tf.losses.sigmoid_cross_entropy(multi_class_labels=binary_maps,
-                                    logits=logits,
-                                    weights=weights,
-                                    label_smoothing=0,
-                                    scope='detector_loss')
-    """
-    # componentwise logistic losses
     losses = tf.nn.sigmoid_cross_entropy_with_logits(labels=binary_maps,
                                                      logits=logits,
                                                      name='cross_entropy_bulat')
 
-	return _compute_weighted_loss(losses, weights, scope='cross_entropy_bulat')
+    losses = _add_weighted_loss_to_collection(losses, weights)
+
 
 def mean_squared_error_loss(logits, endpoints, heatmaps, binary_maps, weights):
     """Currently we regress joint gaussian confidence maps using pixel-wise L2 loss, based on
     Equation 2 of the paper.
     """
-    tf.losses.mean_squared_error(predictions=logits,
-                                 labels=heatmaps,
-                                 weights=weights,
-                                 scope='regressor_loss')
+    losses = tf.square(tf.subtract(logits, heatmaps))
+    losses = _add_weighted_loss_to_collection(losses, weights)
 
 
 # Keeping the in for now for legacy
