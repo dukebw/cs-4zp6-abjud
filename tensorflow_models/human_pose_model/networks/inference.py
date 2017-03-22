added config for vgg and fixed KL loss

tensorflow_models/human_pose_model/networks/inference.py
@@ -14,6 +14,7 @@ from networks import resnet_bulat
 from networks import vgg_vae
 import pdb
 
+
 def _summarize_loss(total_loss, gpu_index):
     """Summarizes the loss and average loss for this tower, and ensures that
     loss averages are computed every time the loss is computed.
@@ -153,23 +154,21 @@ def _mean_squared_error_loss(logits, heatmaps, weights):
 
 
 # These are great mathematicians so their names are written with capital letters ^^
-def _KullbackLeibler(X, weights):
+def _KullbackLeibler(mu, log_sigma):
     """
     (Gaussian) Kullback-Leibler divergence KL(q||p), per training example
     This function can be derived with basic knowledge of Bayesian variational inference,
     see Kingma et al Autoencoding Variational Bayes.
     (thor) Let me first get this running and then I'll explain.
     """
-    # yields a batch of shape [?, width, height, num_classes]
-    #reparam = 1 + 2*log_sigma - mu**2 - tf.exp(2*log_sigma)
-    # Transpose so we get the trace of each channel with tf.trace
-    #reparam_t = tf.transpose(reparam, perm=[0,3,1,2])
-    # take the trace of each channels and reduce the mean across the 16 channels
-    #KL_loss = -0.5 * tf.reduce_mean(tf.trace(reparam_t),1)
-    # reduce the mean across the batch dimension
-    #KL_loss = tf.reduce_mean(KL_loss)
-    KL_loss = tf.reduce_mean(2*X)
-    tf.add_to_collection(name=tf.GraphKeys.LOSSES, value=KL_loss)
+    with tf.name_scope("KL_divergence"):
+        KL = 1 + 2*log_sigma - mu**2 - tf.exp(2*log_sigma)
+        # take the trace of each channels and reduce the mean across the 16 channels
+        KL_loss = -0.5 * tf.reduce_sum(tf.trace(reparam_t),1)
+        # reduce the mean across the batch dimension
+        KL_loss = tf.reduce_mean(KL_loss)
+        # There's no way of assuming the weights to retain meaning wrt latent dimensions
+        tf.add_to_collection(name=tf.GraphKeys.LOSSES, value=KL_loss)
 
 def detector_only_xentropy_loss(logits,
                                 endpoints,
@@ -239,7 +238,7 @@ def vae_detector_loss(logits,
                       is_visible_weights):
     """ Trains the detector as a variational autoencoder
     """
-    _KullbackLeibler(endpoints['gen_img'], weights)
+    _KullbackLeibler(endpoints['z_mu'], endpoints['z_log_sigma'])
 
     _sigmoid_cross_entropy_loss(logits, binary_maps, weights)
 

tensorflow_models/human_pose_model/networks/vgg_vae.py
@@ -97,13 +97,13 @@ def _get_reparameterization(hidden_enc):
     # latent distribution parameterized by hidden encoding
     # z ~ N(z_mean, np.exp(z_log_sigma)**2)
     z_mu = slim.fully_connected(hidden_enc,
-                                NUM_CLASSES,
+                                LATENT_DIM,
                                 activation_fn=None,
                                 normalizer_fn=None,
                                 scope='z_mu')
 
     z_log_sigma = slim.fully_connected(hidden_enc,
-                                       NUM_CLASSES,
+                                       LATENT_DIM,
                                        activation_fn=None,
                                        normalizer_fn=None,
                                        scope='z_log_sigma')
@@ -184,46 +184,17 @@ def _vgg_16_vae_v0(inputs,
                                              dropout_keep_prob,
                                              is_training)
 
-                    latent_var = slim.flatten(a8)
-
-                    X = slim.fully_connected(latent_var,
-                                             NUM_CLASSES,
-                                             activation_fn=None,
-                                             normalizer_fn=None,
-                                             scope='z_mu')
+                    z = slim.flatten(a8)
+                    z_latent, z_mu, z_log_sigma = _get_reparameterization(z)
 
                     pose_logits = _get_pose_logits(a3,a4,a8, inputs.get_shape()[1:3])
                     end_points = slim.utils.convert_collection_to_dict(end_points_collection)
-                    #z_log_sigma = slim.fully_connected(latent_var,
-                    #                                   NUM_CLASSES,
-                    #                                   activation_fn=None,
-                    #                                   normalizer_fn=None,
-                    #                                   scope='z_log_sigma')
-
-
-                    #epsilon = tf.random_normal(tf.shape(z_log_sigma), name="epsilon")
-                    #z = z_mu + 1 * tf.exp(z_log_sigma) # N(mu, I * sigma**2)
-                    #z = sampleGaussian(z_mu, z_log_sigma)
-                    #shape = z.get_shape().as_list()
-                    #size = shape[1]*shape[1]
-                    #print(size)
-                    #d1 = slim.fully_connected(z,
-                    #                          256,
-                    #                          activation_fn=tf.nn.relu,
-                    #                          normalizer_fn=slim.batch_norm,
-                    #                          normalizer_params=batch_norm_params)
-
-                    #d1 = tf.reshape(d1, [9, LATENT_DIM, LATENT_DIM,1])
-                    #d2 = slim.conv2d_transpose(d1,1,2,2)
-                    #d3 = slim.conv2d_transpose(d2,1,2,2)
-                    #gen_img = slim.conv2d_transpose(d3,3,2,2)
-                    #gen_img = _get_img_reconstruction(a8,z,batch_norm_params)
 
                     # Convert end_points_collection into a end_point dict.
                     # For calculating the KL loss
-                    #end_points['z_mu'] = z_mu
-                    #end_points['z_log_sigma'] = z_log_sigma
-                    end_points['gen_img'] = X
+                    end_points['z_mu'] = z_mu
+                    end_points['z_log_sigma'] = z_log_sigma
+                    end_points['pose_logits'] = pose_logits
 
                     return pose_logits, end_points
 